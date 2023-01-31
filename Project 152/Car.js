235ca5bac5481678cf9969e335086600095b742e
SP248 <82733585+SP248@users.noreply.github.com>
2023-01-30 22:23:48 -0500

    Add files via upload

--- Project 152/Car.js
+++ Project 152/Car.js
@@ -1,16 +1,42 @@
-
-AFRAME.registerComponent("click", {
-    schema:{moveX:{type:"number", default:0},
-}}),
-
-update: function() {
-    window.addEventListener("click", e => {
-        this.DataTransfer.clickCounter = this.DataTransfer.clickCounter + 1});
-        if(this.DataTransfer.clickCounter === 1){
-            const rotation = {x: 0, y:20, z: 0 };
-            this.el.setAttribute("rotation", rotation);
-        } else if (this.DataTransfer.clickCounter === 2)
-        {
+AFRAME.registerComponent("car-model", {
+    schema: {
+      modelRef: { type: "string", default: "Ferrari.glb" },
+      clickCounter: { type: "number", default: 0 }
+    },
+    init: function() {
+   
+      this.el.setAttribute("gltf-model", this.data.modelRef);
+      const position = { x: 0, y: 10, z: 80 };
+      const rotation = { x: 0, y: 0, z: 0 };
+      this.el.setAttribute("position", position);
+      this.el.setAttribute("rotation", rotation);
+    },
+    update: function() {
+      window.addEventListener("click", e => {
+        this.data.clickCounter = this.data.clickCounter + 1;
+        if (this.data.clickCounter === 1) {
+          const rotation = { x: 0, y: 20, z: 0 };
+          this.el.setAttribute("rotation", rotation);
+        } else if (this.data.clickCounter === 2) {
+          const rotation = { x: 0, y: 100, z: 0 };
+          this.el.setAttribute("rotation", rotation);
+        } else if (this.data.clickCounter === 3) {
+          const rotation = { x: 0, y: -200, z: 0 };
+          this.el.setAttribute("rotation", rotation);
+        } else if (this.data.clickCounter === 4) {
+          const rotation = { x: 0, y: -100, z: 0 };
+          this.el.setAttribute("rotation", rotation);
+          const cameraEl = document.querySelector("#camera");
+          const position = { x: 0, y: 195, z: 450 };
+          cameraEl.setAttribute("position", position);
+        } else {
+          const rotation = { x: 0, y: -100, z: 0 };
+          this.el.setAttribute("rotation", rotation);
+          const cameraEl = document.querySelector("#camera");
+          const position = { x: 0, y: 50, z: 250 };
+          cameraEl.setAttribute("position", position);
+          this.data.clickCounter = 0;
         }
-      const rotation = {x:0, y:100, z:0};
-
+      });
+    }
+  });
